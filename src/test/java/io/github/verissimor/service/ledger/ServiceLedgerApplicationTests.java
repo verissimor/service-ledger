9791f79d506878a075454ca251c781210cdf99bc
Verissimo Ribeiro <vribeiro@landytech.com>
2025-07-24 12:10:16 +0100

    feat(init): creating the project on spring initializr

---
+++ src/test/java/io/github/verissimor/service/ledger/ServiceLedgerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.verissimor.service.ledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceLedgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
